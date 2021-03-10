b13ce147557c07e1325ed4b15946839b863d1878
milochen0418 <milo_chen@mail2000.com.tw>
2021-03-10 19:38:38 +0800

    2.3 What is the resolver and its role?

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import schema from './schema';
 import graphqlHTTP from 'express-graphql';
+import resolvers from './resolvers';
 
 const app = express();
 
@@ -8,34 +9,7 @@ app.get('/', (req,res) => {
     res.send('Graphql is amazing!');
 });
 
-class Friend {
-    constructor(id, { firstName, lastName, gender, email}) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.email = email;
-    }
-}
-
-const friendDatabase = {};
-
-const root = { 
-    friend: () => {
-        return {
-            "id": 5658489489,
-            "firstName": "Manny",
-            "lastName": "Henri",
-            "gender": "Male",
-            "emails": "me@me.com"
-        }
-    },
-    createFriend: ({input}) => {
-        let id = require('crypto').randomBytes(10).toString('hex');
-        friendDatabase[id] = input;
-        return new Friend(id, input);
-    }
-};
+const root = resolvers;
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
