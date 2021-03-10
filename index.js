466eaf1e8a807d36a99678e6dc3abd0df4941563
milochen0418 <milo_chen@mail2000.com.tw>
2021-03-10 19:31:12 +0800

    2. Types and Schemas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import schema from './schema';
-import { graphqlHTTP } from 'express-graphql';
+import graphqlHTTP from 'express-graphql';
 
 const app = express();
 
